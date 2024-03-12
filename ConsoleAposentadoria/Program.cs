0901fc9f326c6f52283cab8b29eac512330cbcb2
Usuario <Usuario@CPD02-M07>
2024-03-12 12:08:57 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAposentadoria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAposentadoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade em anos que você trabalhou: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            if (idade > 65 && ano > 30 || idade > 60 && ano > 25 ) 
+            {
+                Console.WriteLine("Aposentadoria disponivel"); 
+            }
+            else { Console.WriteLine("Aposentadoria negada"); }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
